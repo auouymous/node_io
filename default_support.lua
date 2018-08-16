96cbee1a3c4236660e90423e9326e651909ee597
auouymous <au@qzx.com>
2018-08-16 03:54:46 -0600

    update furnace to new API and export furnace init function for use by pipeworks

--- default_support.lua
+++ default_support.lua
@@ -10,21 +10,22 @@ if minetest.get_modpath("default") then
 	node_io.init_main_inventory("default:chest_locked", false)
 	node_io.init_main_inventory("default:chest_locked_open", false)
 
-	local function init_furnace(node_name)
+	-- export function so pipeworks can use it to init its replacement furnace
+	node_io.init_default_furnace = function(node_name)
 		local def = {}
 
 		def.node_io_can_put_item = function(pos, node, side) return true end
-		def.node_io_room_for_item = function(pos, node, side, itemstack)
+		def.node_io_room_for_item = function(pos, node, side, itemstack, count)
 			local inv = minetest.get_meta(pos):get_inventory()
-			if not inv then return false end
+			if not inv then return 0 end
 			local inv_name
 			if side == "U" then inv_name = "src" elseif side == "D" then inv_name = "dst" else inv_name = "fuel" end
-			return inv:room_for_item(inv_name, itemstack)
+			return node_io.room_for_item_in_inventory(inv, inv_name, itemstack, count)
 		end
-		def.node_io_put_item = function(pos, node, side, what, itemstack)
+		def.node_io_put_item = function(pos, node, side, putter, itemstack)
 			local inv_name
 			if side == "U" then inv_name = "src" elseif side == "D" then inv_name = "dst" else inv_name = "fuel" end
-			return node_io.put_item_in_inventory(pos, node, inv_name, what, itemstack)
+			return node_io.put_item_in_inventory(pos, node, inv_name, putter, itemstack)
 		end
 
 		def.node_io_can_take_item = function(pos, node, side) return true end
@@ -38,15 +39,20 @@ if minetest.get_modpath("default") then
 			if side == "U" then inv_name = "src" elseif side == "D" then inv_name = "dst" else inv_name = "fuel" end
 			return node_io.get_inventory_name(pos, inv_name, index)
 		end
-		def.node_io_take_item = function(pos, node, side, what, want_item, want_count)
+		def.node_io_get_item_stack = function(pos, node, side, index)
 			local inv_name
 			if side == "U" then inv_name = "src" elseif side == "D" then inv_name = "dst" else inv_name = "fuel" end
-			return node_io.take_item_from_inventory(pos, node, inv_name, what, want_item, want_count)
+			return node_io.get_inventory_stack(pos, inv_name, index)
+		end
+		def.node_io_take_item = function(pos, node, side, taker, want_item, want_count)
+			local inv_name
+			if side == "U" then inv_name = "src" elseif side == "D" then inv_name = "dst" else inv_name = "fuel" end
+			return node_io.take_item_from_inventory(pos, node, inv_name, taker, want_item, want_count)
 		end
 
 		minetest.override_item(node_name, def)
 	end
 	-- furnace
-	init_furnace("default:furnace")
-	init_furnace("default:furnace_active")
+	node_io.init_default_furnace("default:furnace")
+	node_io.init_default_furnace("default:furnace_active")
 end
